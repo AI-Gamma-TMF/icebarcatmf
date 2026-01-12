26d12046b095ad3d137920c847a7e430a85bb826
AMIT DAS <55406328+amitd-dev@users.noreply.github.com>
2026-01-12 15:05:20 -0500

    fix: disable automatic React Query refetching to stop page refreshing

--- frontend/src/reactQuery/queryClientSetup.js
+++ frontend/src/reactQuery/queryClientSetup.js
@@ -1,11 +1,16 @@
 import { QueryClient } from '@tanstack/react-query'
 
 const queryClient = new QueryClient({
-  // defaultOptions: {
-  //   queries: {
-  //     staleTime: Infinity
-  //   }
-  // }
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes - prevent automatic refetch
+      cacheTime: 10 * 60 * 1000, // 10 minutes
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchInterval: false, // Disable all automatic refetching
+    }
+  }
 })
 
 export default queryClient
